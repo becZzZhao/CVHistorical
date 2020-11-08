fce9798d7116c5d10c463e35edc171da0dea7bfd
BecBunny <40709998+BecBunny@users.noreply.github.com>
2020-11-07 19:05:28 -0500

    Update ocr.py
    1. Adding header detection
    2. Putting output into pandas df.
    3. Putting 1, 2 together.

--- code/ocr.py
+++ code/ocr.py
@@ -1,5 +1,5 @@
 
-
+import pandas as pd
 from Horizontal import Horizontal
 from Vertical import Vertical
 import pytesseract
@@ -8,7 +8,7 @@ import cv2
 import numpy as np
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
 import sys
-from Kernel import Kernel
+
 from imutils.perspective import four_point_transform
 from imutils import contours
 import imutils
@@ -19,13 +19,13 @@ from PIL import Image, ImageOps
 
 class OCR(Vertical, Horizontal):
     @staticmethod
-    def get_mask(path_to_raw_page, display = False):
+    def get_mask(binary_image, display = False):
+        # takes binary  and detect vertical& horizontal lines
 
-
-        raw = Page.rawtobinary(path_to_raw_page)
-
+        
+        raw = binary_image.copy()
         # detect horizontal borders
-        dots = Horizontal.find_all_dots(raw, display = False)
+        dots = Horizontal.find_all_dots(binary_image, display = False)
         lines = Horizontal.dotted_to_lines(dots, display=False)
         lines_filtered = Horizontal.filter(lines, len(raw))
         lines_horizontal = Horizontal.populate_lines(lines_filtered, len(raw[0]), len(raw))
@@ -44,8 +44,35 @@ class OCR(Vertical, Horizontal):
         return lines_horizontal,lines_vertical, mask_pic
 
     @staticmethod
+    def split_cells(raw_colored_image, lines_horizontal, lines_vertical, display = False,  topRowIdx = 50, bottomRowIdx = 80, leftColIdx = 0, rightColIdx = 5):
+        # takes raw image (colored) return cropped image of cells/ ROI
+        # output is unprocessed cells. 
+        img = cv2.cvtColor(raw_colored_image, cv2.COLOR_BGR2RGB)
+        cells = []
+        for i in range(topRowIdx,bottomRowIdx):
+            myRow= []
+            for j in range(leftColIdx,rightColIdx):
+                offset = 10
+                top = int(lines_horizontal[i][1])
+                bottom = int(lines_horizontal[i + 1][1]) + offset
+                left = int(lines_vertical[j][0])
+                right = int(lines_vertical[j + 1][0])
 
-    def detect_connected_comp(image):
+                myCell = Page.cropImg([top, bottom, left, right], img)
+                myRow.append(myCell)
+
+                if (bottomRowIdx - topRowIdx == 1) and (rightColIdx - leftColIdx == 1 ):
+                    return myCell
+
+            cells.append(myRow)
+
+        if display == True: Page.show_cell_in_table(cells)
+
+        return cells
+
+    @staticmethod
+    def ROI_to_text(image, col):
+    
         # use MP
         # https://github.com/lavanya-m-k/character-detection-and-crop-from-an-image-using-opencv-python
         image = imutils.resize(image, height=100)
@@ -105,180 +132,15 @@ class OCR(Vertical, Horizontal):
 
 
         # OCR
-        data = pytesseract.image_to_string(result, lang='eng', config='--psm 6')
+        if col in [0, 1, 2, 3]:
+            data = pytesseract.image_to_string(result, lang='eng', config='--psm 6 -c tessedit_char_blacklist=0123456789')
+        else:
+            data = pytesseract.image_to_string(result, lang='eng', config='--psm 6 outputbase digits')
+
         data = data[:-2]
         out = OCR.draw_text(result, data)
 
-        # Page.show_images([myCell, mask, out])
-
-
-        return out
-
-    def remove_trailing_dots_deprec(myCell):
-        np.set_printoptions(threshold=sys.maxsize)
-        # myCell = Page.cropImg([27, 35, 272, 400], myCell)
-        image = myCell
-        #resizing is necessary for better performance. I don't know why
-        image = imutils.resize(image, height=100)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # remove salt-and-pepper noise on the page.
-        thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)[1]
-        # blurring makes the dots more elliptical-like
-        blurred = cv2.blur(thresh, (6, 6), 0)
-        # prepare for contouring
-        edged = cv2.Canny(blurred, 120, 255, 2)
-        # dilation connects the edges
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9,9))
-        edged_dilated = cv2.dilate(edged, kernel)
-        # check unique colors
-        # print(np.unique(image.reshape(-1, image.shape[2]), axis=0))
-        # find the outmost contour
-        cnts = cv2.findContours(edged_dilated, cv2.RETR_CCOMP ,
-                                 cv2.CHAIN_APPROX_NONE)   #cv2.RETR_EXTERNAL
-
-        cnts = imutils.grab_contours(cnts)
-        cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-        # p = Page.create_blankpage(edged)
-        p = image
-
-        circles = []
-        for i in range(0, len(cnts)):
-           (x, y), radius = cv2.minEnclosingCircle(cnts[i])
-           area = cv2.contourArea(cnts[i])
-           circles.append([(x, y), radius, area])
-
-        circles.sort(key = lambda x: x[0][0], reverse = True)
-        print(circles[:14])
-
-        # remove the outliers of the rightmost 3 points.
-
-        for i in range(1, len(circles)):
-            (x0, y0), radius0, area0 = circles[i-1]
-            (x, y), radius, area = circles[i]
-
-            dist_x = abs(x-x0)
-            dist_y = abs(y-y0)
-            dist_euc = (dist_x**2 + dist_y**2)**(1/2)
-            # or (dist_euc > 50)
-            # or (dist_y > 10)
-            if not ((dist_x > 60) ) :
-                # if i > 10:break
-                #
-                if (500> area > 50):
-                # problem: not secure enough to break it, would better off just use houghline
-                    cv2.circle(p, (int(x),int(y)), int(radius), (0, 0, 255), 2)
-                    if (i == 1) & (500 >area0 >50):
-                        cv2.circle(p, (int(x0), int(y0)), int(radius0), (0, 0, 255), 2)
-
-        # Page.show_images([blurred,edged,edged_dilated, p] )
-        return p
-
-
-
-
-
-#https://levelup.gitconnected.com/text-extraction-from-a-table-image-using-pytesseract-and-opencv-3342870691ae
-#https://www.pyimagesearch.com/2018/08/20/opencv-text-detection-east-text-detector/
-    @staticmethod
-    def preprocess_img(myCell):
-        np.set_printoptions(threshold=sys.maxsize)
-        #input is RGB
-
-        # local denoise + binarization
-        myCell = cv2.threshold(myCell, 127, 255, cv2.THRESH_BINARY_INV)[1]
-
-
-        # get unique color to replace
-        # print(np.unique(myCell.reshape(-1, myCell.shape[2]), axis=0))
-
-        # # red & yellow color to white
-        myCell[np.where((myCell == [255, 255, 0]).all(axis=2))] = [255, 255, 255]
-        myCell[np.where((myCell == [255, 0, 0]).all(axis=2))] = [255, 255, 255]
-
-        # convert to binary gray scale (easier to print)
-        myCell = cv2.cvtColor(myCell, cv2.COLOR_RGB2GRAY)
-
-        # initial cropping
-        alph0 = Page.cropImg([8, 31, 100, 135], myCell)
-        dots0 = Page.cropImg([27,35,272,400], myCell)
-
-
-        # erode and dilate
-        kernel = np.array(np.ones((3,4),dtype=np.uint8))  # when erode() takes an empty kernel, it automatically generates a 3*3 rectangular structuring element.
-        kernel = Kernel.padding(kernel)
-        # kernel =  cv2.getStructuringElement(cv2.MORPH_ELLIPSE, ksize=(4,4))
-        dots= cv2.erode(dots0, kernel,iterations=1)  # find all dots that have enough empty spaces around them
-        alph = cv2.erode(alph0, kernel, iterations=1)
-        myCell = cv2.erode(myCell, kernel, iterations=1)
-
-        dots1 = dots.copy()
-
-        kernel2 = np.array(np.ones((2,3),dtype=np.uint8))  # when erode() takes an empty kernel, it automatically generates a 3*3 rectangular structuring element.
-        kernel2 = Kernel.padding(kernel2)
-
-        dots= cv2.dilate(dots, kernel2,iterations=1)  # find all dots that have enough empty spaces around them
-        alph = cv2.dilate(alph, kernel2, iterations=1)
-        myCell = cv2.dilate(myCell, kernel2, iterations=1)
-
-
-        # # erode and dilate
-        # kernel = np.array(np.ones((2,4),dtype=np.uint8))  # when erode() takes an empty kernel, it automatically generates a 3*3 rectangular structuring element.
-        # kernel = Kernel.padding(kernel,size = 2)
-        # # kernel =  cv2.getStructuringElement(cv2.MORPH_ELLIPSE, ksize=(4,4))
-        # dots= cv2.erode(dots0, kernel,iterations=1)  # find all dots that have enough empty spaces around them
-        # alph = cv2.erode(alph0, kernel, iterations=1)
-        # myCell = cv2.erode(myCell, kernel, iterations=1)
-
-
-        # Page.display(myCell)
-        print(dots0)
-        print("<><><><><><><><><><><><><")
-        print(dots1)
-        print("<><><><><><><><><><><><><")
-        print(dots)
-        print("<><><><><><><><><><><><><")
-        print(alph)
-
-        Page.display(myCell)
-
-
-
-        return dots
-        # Page.display(dots)
-        # dots= cv2.dilate(dots, kernel, myPoint, iterations=5)
-
-        # myCell = Horizontal.dotted_to_lines(myCell)
-
-
-
-
-    @staticmethod
-    def split_cells(img, lines_horizontal, lines_vertical, topRowIdx = 50, bottomRowIdx = 80, leftColIdx = 0, rightColIdx = 5):
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        cells = []
-        for i in range(topRowIdx,bottomRowIdx):
-            myRow= []
-            for j in range(leftColIdx,rightColIdx):
-                offset = 10
-                top = int(lines_horizontal[i][1])
-                bottom = int(lines_horizontal[i + 1][1]) + offset
-                left = int(lines_vertical[j][0])
-                right = int(lines_vertical[j + 1][0])
-
-                myCell = Page.cropImg([top, bottom, left, right], img)
-                myRow.append(myCell)
-
-                if (bottomRowIdx - topRowIdx == 1) and (rightColIdx - leftColIdx == 1 ):
-                    return myCell
-
-            cells.append(myRow)
-
-
-        return cells
-
-
-
-
+        return data, out
 
     @staticmethod
     def draw_text(img, text):
@@ -302,5 +164,99 @@ class OCR(Vertical, Horizontal):
                     fontColor,
                     lineType)
         return labeled_img
+    
+
+    @staticmethod
+    def Page_to_DF(colored, binary_page, display = False):
+        # this function assembles split_cells, ROI to text, returns a df
+
+        lines_horizontal, lines_vertical, mask_pic = OCR.get_mask(binary_page, display=False)
+        cells = OCR.split_cells(colored, lines_horizontal, lines_vertical, display=False, topRowIdx=50, bottomRowIdx=55,
+                                leftColIdx=0, rightColIdx=5)
 
 
+        table = cells.copy()
+        out = []
+        out_pic = []
+        for row in table:
+            myRowOut = []
+            myRowOut_pic = []
+            col = 0
+            for cell in row:
+                data, img = OCR.ROI_to_text(cell, col = col)
+                myRowOut.append(data)
+                myRowOut_pic.append(img)
+                col += 1
+
+            out.append(myRowOut)
+            out_pic.append(myRowOut_pic)
+
+        out = pd.DataFrame(out)
+
+        if display == True: 
+            Page.show_cell_in_table(out_pic)
+
+        return out
+
+
+    @staticmethod
+    def get_header_ROIs(colored_nparray, binary_page, display = True):
+        colored_nparray = cv2.cvtColor(colored_nparray, cv2.COLOR_BGR2RGB) # turn it blue, basically, to integrate with the ROI_to_text method
+        # binary_page = Page.colored_to_binary(colored_nparray)
+        # takes a binary pate, crops out the top 1/5 of it.  
+        # methodology, dilate and blurr text regions into rectangle-like objects
+        # then detect these objects as ROI
+        # problem: horizontal lines become noises, at the moment, only used area as a filtering mechanism. This could be improved by Houghline > remove.
+        
+        binary_page = Page.cropImg([0, int(len(binary_page)/5), 0, len(binary_page[0])], binary_page)
+
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4, 4))
+        eroded= cv2.erode(binary_page, kernel)
+    
+        kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+        dilated = cv2.dilate(eroded, kernel2)
+        kernel3 = cv2.getStructuringElement(cv2.MORPH_RECT, (50,5))
+        dilated = cv2.dilate(dilated, kernel3)
+        kernel4 = cv2.getStructuringElement(cv2.MORPH_RECT, (6,30))
+        dilated = cv2.dilate(dilated, kernel4)
+    
+        blurred = cv2.blur(dilated, (50, 50), 30)
+
+    
+        dilated = dilated.astype('uint8')
+        contours, hierarchy = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+        ROIs = []
+        # out_illustrate = binary_page.copy()
+        for cnt in contours:
+            x, y, w, h = cv2.boundingRect(cnt)
+            if (cv2.contourArea(cnt) > 15000) and ( 1000>w > 500):
+                # cv2.rectangle(out_illustrate, (x, y), (x + w, y + h), (255, 0, 0), 3)
+                top, bottom, left, right = y, y+h, x, x+w
+                image = Page.cropImg([top,bottom, left, right], colored_nparray)
+                ROIs.append(image)
+
+        header_info = []
+        out_pic = []
+        for ROI in ROIs:
+            data, img = OCR.ROI_to_text(ROI, col= 1) # means that it will be recognized 
+            header_info.append(data)
+            out_pic.append(img)
+
+        print(header_info)
+        if display == True: Page.show_images(out_pic)
+
+        return header_info
+        
+
+    @staticmethod
+    def page_to_df_and_header(path, display = False):
+        colored = Page.load_colored(path)
+        binary_page = Page.colored_to_binary(colored)
+        header_info = OCR.get_header_ROIs(colored, binary_page, display = display)
+        df = OCR.Page_to_DF(colored, binary_page, display = display)
+        
+        print(header_info)
+        print(df)
+        
+        return header_info, df
